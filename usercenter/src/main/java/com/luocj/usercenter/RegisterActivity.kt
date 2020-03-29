7efeb39443447fc466aadc55ed9dbe134dc6dbd3
289759379@qq.com <609432148>
2020-03-29 11:32:25 +0800

    Initial commit

---
+++ usercenter/src/main/java/com/luocj/usercenter/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.luocj.usercenter
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
